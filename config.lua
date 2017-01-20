9ca929f337b948ce5e7c91a4d1630ac6307f4f7d
mahditalbi <mahdi.4afm@gmail.com>
2017-01-20 13:22:57 +0330

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
-bot_token = "183531221:AAHqvpuAkeeOEwkdtTT2g4RC87QhUUDD9es"
+bot_token = "314402642:AAH1-olvYWp-zXbpSwnvsQZtAWcwnMsuTPI"
 send_api = "https://api.telegram.org/bot"..bot_token
 bot_version = "1.0"
-admingp = -129471512
+admingp = -327309389
